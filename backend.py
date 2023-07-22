3e27d7074f1b2c92175f9daf2a708358bf67a5be
infernostars <blazem7708.2@gmail.com>
2023-07-22 12:35:15 -0600

    add RepeatedTimer utility class

--- backend.py
+++ backend.py
@@ -2,9 +2,11 @@ import configparser
 import os
 import random
 import sys
+from typing import Callable, Any
 import discord
 import logging
 import inflect
+import threading
 import pathlib
 from colorlog import ColoredFormatter
 
@@ -152,3 +154,35 @@ def name_generator(kind: str, amount: int = 10) -> list[str]:
 def plural(text, num):
     """Wrapper for Inflect's automatic plurals."""
     return p.plural(text, num)
+
+
+class RepeatedTimer:
+    """Utility class to repeat a function every [interval] seconds.
+     Starts on creation, stoppable by stop() and startable with start()."""
+    def __init__(self, interval: float, function: Callable, *args: Any, **kwargs: Any):
+        self._timer = None
+        self.interval = interval
+        self.function = function
+        self.args = args
+        self.kwargs = kwargs
+        self.is_running = False
+
+    def _run(self):
+        self.is_running = False
+        self.start()  # Reschedule the timer for the next run
+        self.function(*self.args, **self.kwargs)
+
+    def start(self):
+        """Start or restart the RepeatedTimer."""
+        if not self.is_running:
+            # Schedule the next run of the function
+            self._timer = threading.Timer(self.interval, self._run)
+            self._timer.start()
+            self.is_running = True
+
+    def stop(self):
+        """Pause or stop the RepeatedTimer."""
+        # Cancel the timer if it is currently running
+        if self._timer:
+            self._timer.cancel()
+        self.is_running = False
